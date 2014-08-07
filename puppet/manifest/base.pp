a8237ed447a16ceed1308a1b4287ea7bc8e1c7d9
Zachary Davy <zacharyjdavy@gmail.com>
2014-08-06 22:41:46 -0700

    added manifest

---
+++ puppet/manifest/base.pp
@@ -0,0 +1,12 @@
+class { 'java':
+  distribution => 'jdk',
+  version      => 'latest',
+}
+
+maven::setup { "maven":
+  ensure        => 'present',
+  source        => 'apache-maven-3.1.1-bin.tar.gz',
+  deploymentdir => '/home/vagrant/apache-maven',
+  user          => 'vagrant',
+  pathfile      => '/home/vagrant/.bashrc'
+}
