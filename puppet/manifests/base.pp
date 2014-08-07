8a6f08e7076a3881b9af5e469f2209e5bb6892fd
Zachary Davy <zacharyjdavy@gmail.com>
2014-08-07 14:28:27 -0700

    wip

--- puppet/manifests/base.pp
+++ puppet/manifests/base.pp
@@ -1,12 +1,12 @@
 class { 'java':
   distribution => 'jdk',
   version      => 'latest',
-}
+ }
 
 maven::setup { "maven":
   ensure        => 'present',
   source        => 'apache-maven-3.1.1-bin.tar.gz',
-  deploymentdir => '/home/vagrant/apache-maven',
+  deploymentdir => '/home/vagrant/apps/apache-maven',
   user          => 'vagrant',
   pathfile      => '/home/vagrant/.bashrc'
-}
+ }
